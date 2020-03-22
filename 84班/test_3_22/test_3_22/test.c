31efd583ac22cbaede7156d0e50d8bd17fc5664a
Peter <zpengwei@yeah.net>
2020-03-22 18:09:08 +0800

    更新代码和板书

---
+++ 84班/test_3_22/test_3_22/test.c
@@ -0,0 +1,360 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+//
+//int main()
+//{
+//	short s = 0;
+//	int a = 10;
+//	printf("%d\n", sizeof(s = a + 5));
+//	printf("%d\n", s);
+//
+//	//int a = 0;
+//	//char b = 'w';
+//	//int arr[10] = { 0 };
+//
+//	//printf("%d\n", sizeof a);
+//	//printf("%d\n", sizeof(int));
+//
+//	//printf("%d\n", sizeof(b));
+//	//printf("%d\n", sizeof(char));
+//
+//	//printf("%d\n", sizeof(arr));//10*4 = 40
+//	//printf("%d\n", sizeof(int [10]));
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a = 11;
+//	a = a | (1 << 2);
+//	printf("%d\n", a);//15
+//	a = a & (~(1<<2));
+//	printf("%d\n", a);//11
+//
+//	//0000000000000000000000000000001011
+//	//0000000000000000000000000000000100
+//	//1<<2;
+//	//0000000000000000000000000000000001
+//	//
+//	//0000000000000000000000000000001111
+//	//1111111111111111111111111111111011
+//	//0000000000000000000000000000000100
+//	//0000000000000000000000000000001011
+//	//
+//	//
+//	//int a = 0;
+//	////~ 按（2进制）位取反
+//	////00000000000000000000000000000000
+//	////11111111111111111111111111111111 - 补码
+//	////11111111111111111111111111111110 - 反码
+//	////10000000000000000000000000000001 - 原码
+//	////-1
+//	//printf("%d\n", ~a);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	//printf("%d\n", ++a);//前置++，先++，后使用
+//	printf("%d\n", a++);//后置++，先使用，再++
+//	
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = (int)3.14;//int a = int(3.14);//err
+//
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//void test1(int arr[])
+//{
+//	printf("%d\n", sizeof(arr));//(2)
+//}
+//void test2(char ch[])
+//{
+//	printf("%d\n", sizeof(ch));//(4)
+//}
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	char ch[10] = { 0 };
+//	printf("%d\n", sizeof(arr));//(1)
+//	printf("%d\n", sizeof(ch));//(3)
+//	test1(arr);
+//	test2(ch);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 0;
+//	int b = 5;
+//	int c = a || b;
+//	printf("%d\n", c);
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0, a = 1, b = 2, c = 3, d = 4;
+//	//i = a++ && ++b && d++;
+//	i = a++||++b||d++;
+//
+//	printf("a = %d\n b = %d\n c = %d\nd = %d\n", a, b, c, d);
+//	//
+//	return 0;
+//}
+////程序输出的结果是什么？
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = 0;
+//
+//	if (a > b)
+//		max = a;
+//	else
+//		max = b;
+//
+//	max = (a > b ? a : b);
+//	
+//	//if (a > 5)
+//	//	b = 3;
+//	//else
+//	//	b = -3;
+//
+//	//b = (a > 5 ? 3 : -3);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a[10] = { 0 };
+//	a[4] = 10;
+//	1 + 2;
+//	return 0;
+//}
+//
+//int get_max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	//调用函数的时候的()就是函数调用操作符
+//	int max = get_max(a, b);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
+
+//学生
+//int float
+//
+//创建一个结构体类型-struct Stu
+//struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	int age;
+//	char id[20];
+//};
+//
+//int main()
+//{
+//	int a = 10;
+//	//使用struct Stu这个类型创建了一个学生对象s1，并初始化
+//	struct Stu s1 = {"张三", 20, "2019010305"};
+//	struct Stu* ps = &s1;
+//
+//	printf("%s\n", ps->name);
+//	printf("%d\n", ps->age);
+//	//结构体指针->成员名
+//
+//	/*printf("%s\n", (*ps).name);
+//	printf("%d\n", (*ps).age);*/
+//
+//	/*printf("%s\n", s1.name);
+//	printf("%d\n", s1.age);
+//	printf("%s\n", s1.id);*/
+//	//结构体变量.成员名
+//	return 0;
+//}
+//
+//int main()
+//{
+//	char a = 3;
+//	//00000000000000000000000000000011
+//	//00000011 - a
+//	//
+//	char b = 127;
+//	//‭00000000000000000000000001111111
+//	//‬01111111 - b
+//
+//	//a和b如何相加
+//	//00000000000000000000000000000011
+//	//00000000000000000000000001111111
+//	//00000000000000000000000010000010
+//	//
+//	char c = a + b;
+//	//10000010-c
+//	//11111111111111111111111110000010 - 补码
+//	//11111111111111111111111110000001 - 反码
+//	//10000000000000000000000001111110- 原码
+//	//-126
+//	printf("%d\n", c);
+//
+//	return 0;
+//}
+
+//实例1
+//int main()
+//{
+//	char a = 0xb6;
+//	short b = 0xb600;
+//	int c = 0xb6000000;
+//	if (a == 0xb6)
+//		printf("a");
+//	if (b == 0xb600)
+//		printf("b");
+//	if (c == 0xb6000000)
+//		printf("c");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char c = 1;
+//	printf("%u\n", sizeof(c));//1
+//	printf("%u\n", sizeof(+c));//4
+//	printf("%u\n", sizeof(!c));//1
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = b + a + 3;
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int i = 1;
+//	int a = (++i) + (++i) + (++i);
+//
+//	printf("a = %d\n", a);
+//}
+//
+//
+
+
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;//指针变量
+//	return 0;
+//}
+//
+//int main()
+//{
+//	//printf("%d\n", sizeof(char*));
+//	//printf("%d\n", sizeof(short*));
+//	//printf("%d\n", sizeof(int*));
+//	//printf("%d\n", sizeof(double*));
+//
+//	int a = 0x11223344;
+//	//int* pa = &a;
+//	//*pa = 0;
+//	char* pc = &a;
+//	*pc = 0;
+//	//printf("%p\n", pa);
+//	//printf("%p\n", pc);
+//
+//	return 0;
+//}
+//
+
+
+//
+//int main()
+//{
+//	int a = 0x11223344;
+//	int* pa = &a;
+//	char* pc = &a;
+//	printf("%p\n", pa);
+//	printf("%p\n", pa+1);
+//
+//	printf("%p\n", pc);
+//	printf("%p\n", pc+1);
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	//int* p = arr;//数组名-首元素的地址
+//	char*p = arr;
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		*(p + i) = 1;
+//	}
+//
+//	return 0;
+//}
+//
+//int main()
+//{
+//	//int a;//局部变量不初始化，默认是随机值
+//	int *p;//局部的指针变量，就被初始化随机值
+//	*p = 20;
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int *p = arr;
+//	int i = 0;
+//	for (i = 0; i < 12; i++)
+//	{
+//		p++;
+//	}
+//	//
+//	return 0;
+//}
+
+int* test()
+{
+	int a = 10;
+	return &a;
+}
+int main()
+{
+	int *p = test();
+	*p = 20;
+
+	return 0;
+}

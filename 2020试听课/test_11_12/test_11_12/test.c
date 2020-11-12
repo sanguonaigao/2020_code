52daff733870e9f78edc1c44f25b3410ff00f886
Peter <zpengwei@yeah.net>
2020-11-12 21:25:45 +0800

    更新代码和板书

--- 2020试听课/test_11_12/test_11_12/test.c
+++ 2020试听课/test_11_12/test_11_12/test.c
@@ -2,64 +2,70 @@
 
 #include <stdio.h>
 
-////主函数-程序的入口
+//主函数-程序的入口
 //int main()
 //{
-//	//库函数-
-//	printf("hello world\n");
+//	//打印函数-库函数
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//	printf("hehe\n");
+//
+//
 //	return 0;
 //}
 
 
+
 //int main()
 //{
-//	int a = 0;
-//	int b = 0;
-//	int c = 0;
+//	int a = 0;//3
+//	int b = 0;//5
 //	scanf("%d%d", &a, &b);
 //	//交换
-//	printf("a=%d b=%d\n", a, b);
+//	int c = 0;
+//	printf("交换前：a=%d b=%d\n", a, b);
 //	c = a;
 //	a = b;
 //	b = c;
-//	printf("a=%d b=%d\n", a, b);
+//	printf("交换后：a=%d b=%d\n", a, b);
 //
 //	return 0;
 //}
+//
 
 
 //int main()
 //{
-//	int a = 0;
-//	int b = 0;
+//	int a = 0;//3
+//	int b = 0;//5
 //	scanf("%d%d", &a, &b);
 //	//交换
 //	printf("交换前：a=%d b=%d\n", a, b);
-//
 //	a = a + b;
 //	b = a - b;
 //	a = a - b;
-//
 //	printf("交换后：a=%d b=%d\n", a, b);
 //
 //	return 0;
 //}
 //
+
 //
 //int main()
 //{
-//	int a = 0;
-//	int b = 0;
+//	int a = 0;//3
+//	int b = 0;//5
 //	scanf("%d%d", &a, &b);
 //	//交换
 //	printf("交换前：a=%d b=%d\n", a, b);
-//	//^ - 按（2进制）位异或
-//	//想同为0，相异为1
-//
+//	//^ 异或 - 按（2进制）位异或
+//	//相同为0，相异为1
 //	a = a^b;
 //	b = a^b;
 //	a = a^b;
-//
 //	printf("交换后：a=%d b=%d\n", a, b);
 //
 //	return 0;
@@ -67,15 +73,16 @@
 //
 
 
+//暴力求解
 //int main()
 //{
-//	int arr[] = { 8, 2, 3, 4, 5, 8, 2, 3, 4 ,7,5};
-//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int arr[] = { 8, 2, 3, 4, 5, 8, 2, 3, 4,7,5};
+//	int sz = sizeof(arr) / sizeof(arr[0]);//计算数组的元素个数
 //	int i = 0;
 //	for (i = 0; i < sz; i++)
 //	{
 //		int count = 0;
-//		//统计arr[i]在arr数组中出现的次数
+//		//统计一下arr[i]在arr数组中出现的次数
 //		int j = 0;
 //		for (j = 0; j < sz; j++)
 //		{
@@ -84,7 +91,7 @@
 //				count++;
 //			}
 //		}
-//		if (1 == count)
+//		if (count == 1)
 //		{
 //			printf("单身狗是：%d\n", arr[i]);
 //			break;
@@ -93,68 +100,37 @@
 //	return 0;
 //}
 
+//3^3=0
 //a^a=0
 //0^a=a
-//异或支持交换律
-
 //3^3^5=5
 //3^5^3=5
-//
+//异或操作符是支持交换律的
+
 //int main()
 //{
-//	int arr[] = { 1,2,3,4,5,1,2,3,4,7,5};
-//
-//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int arr[] = { 8, 2, 3, 4, 5, 8, 2, 3, 4,7,5};
+//	int sz = sizeof(arr) / sizeof(arr[0]);//计算数组的元素个数
 //	int i = 0;
 //	int ret = 0;
-//
 //	for (i = 0; i < sz; i++)
 //	{
-//		ret = ret ^arr[i];
+//		ret = ret ^ arr[i];
 //	}
-//
 //	printf("单身狗是：%d\n", ret);
-//
 //	return 0;
 //}
 
 #include <string.h>
 #include <stdlib.h>
 
-int my_strlen(const char*p)
-
-{
-	int i = 0;
-	while (*p != '\0')
-	{
-
-		p++;
-
-		i++;
-
-	}
-
-	return i;
-
-
-
-}
-
-int main()
-
-{
-	const char*name = { "hello world!" };
-	printf("%d\n", my_strlen(name));
-	return 0;
-}
-
 //int main()
 //{
-//	char input[20] = {0};
-//	system("shutdown -s -t 60");//指向系统命令
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 60");//执行系统命令的
 //again:
-//	printf("请注意，你的电脑在1分钟内关机，如果输入：我是猪，就取消\n");
-//	scanf("%s", input);
+//	printf("请注意，你的电脑在1分钟内关机，如果输入：我是猪，就取消关机\n");
+//	scanf("%s", input);//
 //	if (strcmp(input, "我是猪") == 0)
 //	{
 //		system("shutdown -a");
@@ -165,5 +141,9 @@ int main()
 //	}
 //	return 0;
 //}
-
+//
+//
+//
+//
+//
 

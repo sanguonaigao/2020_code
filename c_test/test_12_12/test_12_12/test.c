5d4044fa75a3292eff49bd5c8dcb98c97af9a4c3
Peter <zpengwei@yeah.net>
2020-12-12 20:48:52 +0800

    更新代码和板书

---
+++ c_test/test_12_12/test_12_12/test.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("a=%d b=%d c=%d\n", a, b, c);
+//
+//	return 0;
+//}
+
+int main()
+{
+	char arr[10] = { 0 };//
+	scanf("%s", arr);
+	printf("%s\n", arr);
+	return 0;
+}
